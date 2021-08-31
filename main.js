c62a2fb2d0ef6b39033ca355245c4a0b6e6b7def
Barret-Wallace <85964411+Barret-Wallace@users.noreply.github.com>
2021-08-31 16:30:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function Start(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/z_9g8_Snq/model.json", modelReady);
+}
+
+function modelReady(){
+    console.log("Model is Ready!");
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, result){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log("got result")
+    }
+}
